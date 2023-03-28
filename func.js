736d8909755392a4f356f474749f1a5df9c3cbe5
HARSH <mishraharshmishra2020@gmail.com>
2023-03-29 01:35:36 +0530

    first commit

---
+++ func.js
@@ -0,0 +1,20 @@
+console.log("This is function tutorial in JS");
+
+function greet(name , greettext = "Greetings from JavaScript"){
+    let name1 ="name1";
+    console.log(greettext+ "  " + name );
+    console.log(name + " is a good boy");
+}
+let name= "HARSH";
+let name1 = "Nikhil";
+let name2 = "Sundaram";
+let name3 = "Karuna";
+let greettext =" Good Morning";
+greet(name , greettext);
+greet(name1, greettext);
+greet(name2, greettext);
+greet(name3 );
+// console.log(name+" is a very good boy");
+// console.log(name1+" is a my loving son");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good boy");

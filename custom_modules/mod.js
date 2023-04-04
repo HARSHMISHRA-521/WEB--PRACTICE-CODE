0f5ffb3af8aa58cbe652b85b3348f14f14f50d6d
HARSH <mishraharshmishra2020@gmail.com>
2023-04-05 02:53:55 +0530

    custom_module

---
+++ custom_modules/mod.js
@@ -0,0 +1,16 @@
+console.log("This is module");
+function average(arr){
+    let sum =0;
+    arr.forEach(element => {
+        sum+=element;
+    });
+    return sum/arr.length;
+}
+
+// module.exports = {
+//     avg:average,
+//     name:"Harsh",
+//     repo:"GitHub"
+// }
+
+module.exports.name = "Harsh";

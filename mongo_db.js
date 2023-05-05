8a8e677c603751e69b1cdfecc5a56b19e2b58190
HARSH <mishraharshmishra2020@gmail.com>
2023-05-06 03:44:09 +0530

    mongo db commands

--- mongo_db.js
+++ mongo_db.js
@@ -1,5 +1,5 @@
 //inserting data in mongo db
-use harsh
-db.items.insertOne({name:"samsung",price:15000,rating: 4.5, qty: 233,sold: 98})
+// use harsh
+// db.items.insertOne({name:"samsung",price:15000,rating: 4.5, qty: 233,sold: 98})
 
-db.items.insertMany([{ name: "Samsung 30s", price: 22000, rating: 4.5, qty: 233, sold: 98 }, { name: "Moto 30s", price: 29000, rating: 3.5, qty: 133, sold: 598 }, { name: "Realme 80s", price: 129000, rating: 2.5, qty: 633, sold: 98, isBig: true }])
+// db.items.insertMany([{ name: "Samsung 30s", price: 22000, rating: 4.5, qty: 233, sold: 98 }, { name: "Moto 30s", price: 29000, rating: 3.5, qty: 133, sold: 598 }, { name: "Realme 80s", price: 129000, rating: 2.5, qty: 633, sold: 98, isBig: true }])

bb7ff5835eca62da88ba58acdccff31cd16320f3
HARSH <mishraharshmishra2020@gmail.com>
2023-05-09 20:39:11 +0530

    updation in mongoDB

---
+++ updation_mongoDB.js
@@ -0,0 +1,8 @@
+// show dbs 
+// use harsh
+// show collections
+
+// db.items.find()
+// db.items.updateOne({ name: "Moto 30s" }, { $set: { price: 2 } })
+// db.items.find()
+// db.items.updateMany({ name: "Moto 30s" }, { $set: { price: 3, rating: 1 } })

bfd93d53462a118a20639f98c8fb4b003f7e1ac2
HARSH <mishraharshmishra2020@gmail.com>
2023-05-09 20:11:43 +0530

    deletion in mongoDB

---
+++ deletion_mongoDB.js
@@ -0,0 +1,15 @@
+// show dbs 
+// use harsh
+// show collections
+
+// db.items.find({ price: 22000 })
+
+// // Deleting items from the Mongo Database
+
+// // deleteOne will delete the matching document entry and will delete the first entry in case of multi document match
+// db.items.deleteOne({ price: 22000 })
+
+
+// // deleteMany will delete all the matching document entry  in case of multi document match
+// db.items.deleteMany({ price: 129000 })
+
